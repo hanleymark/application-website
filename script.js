9229436ac9877b4ba177130c8d01236590a4d887
Mark hanley <home@markhanley.co.uk>
2022-11-17 13:34:28 +0000

    Set up multiple viewport triggered animations

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-const typeText = document.querySelector("#code");
-const typeTextContent = typeText.textContent;
-let timer = null;
-typeText.textContent = "";
+// Get references to all 'typing code' style  animated banners
+const typingTextElements = document.querySelectorAll(".code");
+// Set up arrays to keep track of contents, interval timers and character iterators in the banners
+let typingTextContents = [];
+let typingTextTimers = [];
+let typingTextIterators = [];
 
+setUpTypingAnimation(typingTextElements);
+
+// When user scrolls, check each code typing animation to see if it is in the viewport
+// If it is, start its animation
 window.addEventListener('scroll', function (event) {
-    if (isInViewPort(typeText)) {
-        animateCode();
+    for (let i = 0; i < typingTextElements.length; i++) {
+        if (isInViewPort(typingTextElements[i])) {
+            animateTyping(i);
+        }
     }
 }, false);
 
-function animateCode() {
-    let pos = 1;
-    if (timer == null) {
-        clearInterval(timer);
-        timer = setInterval(frame, 140);
-        console.log("In animateCode()");
+// Check if typing animation has been started, if not start it
+function animateTyping(index) {
+    if (typingTextTimers[index] == null) {
+        typingTextTimers[index] = setInterval(frame, 140);
     }
-
+    // Iterate through each letter of the typing animation using its iterator
     function frame() {
-        if (pos <= typeTextContent.length) {
-            typeText.innerHTML = typeTextContent.substring(0, pos);// + '<span id="cursor"></span>';
-            pos++;
+        if (typingTextIterators[index] <= typingTextContents[index].length) {
+            typingTextElements[index].innerText = typingTextContents[index].substring(0, typingTextIterators[index]);
+            typingTextIterators[index]++;
         }
         else {
-            clearInterval(timer);
+            // If animation is complete, clear the interval timer
+            clearInterval(typingTextTimers[index]);
         }
     }
 }
 
+// Function checks if supplied element parameter is in the current viewport
 function isInViewPort(element) {
     var elementRectangle = element.getBoundingClientRect();
     return (
@@ -37,3 +45,13 @@ function isInViewPort(element) {
         elementRectangle.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 };
+
+// Initialise typing banners
+function setUpTypingAnimation(elements) {
+    elements.forEach(element => {
+        typingTextContents.push(element.innerText);
+        typingTextTimers.push(null);
+        typingTextIterators.push(0);
+        element.innerText = "";
+    });
+}
